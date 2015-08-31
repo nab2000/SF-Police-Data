c944bbd6c926aaedb8525d638fede9818d02daca
unknown <NDR@Dell-CKP5CS1.FEDOM.com>
2015-08-31 17:23:52 -0400

    Additional of Month Category

--- SF_Cleaning.R
+++ SF_Cleaning.R
@@ -8,24 +8,32 @@ SF_clean <- function(file_name = "train.csv", dir = "C:/Users/ndr/Documents/Proj
     train$Year <- as.Date(train[,1])
     train$Year <- as.numeric(format(train$Year, "%Y"))
     
+    train$Month <- as.Date(train[,1])
+    train$Month <- format(train$Month, "%b")
+    
     clean_df <- as.data.frame(NULL)
     row_no <- 0
-    for (w in 1: length(unique(train$Year))){
-        year <- unique(train$Year)[w]
+    for (a in 1: length(unique(train$Month))) {
+        month <- unique(train$Month)[a]
         
-        for (x in 1:length(unique(train$DayOfWeek))){
-            dow <- unique(train$DayOfWeek)[x]
-        
-            for (z in 1:length(unique(train$PdDistrict))) {
-                pd <- unique(train$PdDistrict)[z]
-                row_no <- row_no + 1
-                clean_df[row_no, 1] <- dow
-                clean_df[row_no, 2] <- pd
-                clean_df[row_no, 3] <- year
-                sum <- summary(train$Category[train$DayOfWeek == dow & train$PdDistrict == pd & train$Year == year])
-        
-                for (y in 1:length(sum)) {
-                    clean_df[row_no, y+3] <- sum[y]
+        for (w in 1: length(unique(train$Year))){
+            year <- unique(train$Year)[w]
+            
+            for (x in 1:length(unique(train$DayOfWeek))){
+                dow <- unique(train$DayOfWeek)[x]
+            
+                for (z in 1:length(unique(train$PdDistrict))) {
+                    pd <- unique(train$PdDistrict)[z]
+                    row_no <- row_no + 1
+                    clean_df[row_no, 1] <- dow
+                    clean_df[row_no, 2] <- pd
+                    clean_df[row_no, 3] <- year
+                    clean_df[row_no, 4] <- month
+                    sum <- summary(train$Category[train$DayOfWeek == dow & train$PdDistrict == pd & train$Year == year & train$Month == month])
+            
+                    for (y in 1:length(sum)) {
+                        clean_df[row_no, y+4] <- sum[y]
+                    }
                 }
             }
         }
