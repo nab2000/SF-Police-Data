0ebdd632af6f48b842de962d114e4772e96a9698
nab2000 <nick.dean.rose@gmail.com>
2015-09-01 13:59:50 -0400

    Develop sapply function for quicker_clean function

--- quicker_cleaning.R
+++ quicker_cleaning.R
@@ -1,3 +1,35 @@
-a <- with( sub, sapply(split(sub$Category, list(sub$DayOfWeek, sub$PdDistrict)), summary))
+SF_clean <- function(file_name = "train.csv", vars = c("Month", "Year", "DayOfWeek", "PdDistrict"), dir = "C:/Users/ndr/Documents/Projects/R Projects/SF Crime/", 
+                     save_as = "clean_df"){
+## pulls in data and then summarizes crime category by vars
+    
+    file_loc <- paste(dir, file_name, sep = "")
+    
+    if (exists(file_name)) {
+        train <<- get(file_name)
+        
+    } 
+    else if (file.exists(file_loc)){
+        train <<- read.csv(file_loc, check.names = F)
+        
+    }
+    
+    else { print("file does not exist"); stop }
+    
+    train$Year <- as.Date(train[,1])
+    train$Year <- as.numeric(format(train$Year, "%Y"))
+    
+    train$Month <- as.Date(train[,1])
+    train$Month <- format(train$Month, "%b")
 
-b <- with( sub, tapply(sub$Category, list(sub$DayOfWeek, sub$PdDistrict), summary))
+
+
+
+sort_df <- sapply(split(train$Category, train[,vars]), summary)
+
+sort_df <<-sort_df
+
+## converts sort_df into a more usable data frame for use with prob_table function
+
+}
+
+
