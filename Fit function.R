4c251fc81817f35f5ab296e57c9ea10bd7fe11e1
nab2000 <nick.dean.rose@gmail.com>
2015-10-02 08:41:01 -0400

    Finished use with for loop, function still not working

--- Fit function.R
+++ Fit function.R
@@ -1,7 +1,7 @@
 ##fitting loop
-SF_Analy <- function(file_name = "test", prob_tbl = prob_table, 
+SF_Analy <- function(file_name = "test", prob_tbl = "prob_table", 
                      save_as = "results", date_format = TRUE,
-                     dir = "./", ){
+                     dir = "./"){
     ## check if given file_name is a data frame or file
     file_loc <- paste (dir, file_name, ".csv", sep ="")
     
@@ -16,38 +16,38 @@ SF_Analy <- function(file_name = "test", prob_tbl = prob_table,
     
     if (exists(prob_tbl)) {} 
     
-    else if (file.exists(file_loc)){prob_tbl <<- read.csv(file_loc, check.names 
-                                                          = False)} 
+    else if (file.exists(file_loc)){prob_tbl <<- read.csv(file_loc, 
+                                                          check.names= FALSE)} 
     
     else { print("file does not exist"); stop }
     
     ## turn date and time column into year column
-    if(date_format){
-        test$Year <- as.Date(test[, "Dates"])
-        test$Year <- as.numeric(format(test$Year, "%Y"))
-        test$Month <- as.Date(test[,"Dates"])
-        test$Month <- format(test$Month, "%b")
-    }
+    test$Year <<- as.Date(test[, "Dates"])
+    test$Year <<- as.numeric(format(test$Year, "%Y"))
+    test$Month <<- as.Date(test[,"Dates"])
+    test$Month <<- format(test$Month, "%b")
  
     ##generate linear model from Prob_tbl      
     for(i in 1:39){
         col_no <- i+4
         var_name <- colnames(prob_tbl)[col_no]
-        fit_name <- paste(var_name)
+        fit_name <- paste(var_name, "ft", sep="")
         assign(fit_name, lm(get(var_name)~Year + PdDistrict + Month + DayOfWeek, 
                              data = prob_tbl))
         
     }
-    results_df <- data.frame(col_1 = rep(NA, nrow(test)))
+    results_df <- data.frame(Id = test[,1])
 
     ## apply linnear models 
     for (i in 1:39){
         col_no <- i+4
         var_name <- colnames(prob_tbl)[col_no]
-        col <-     predict(get(var_name), test)
+        fit_name <- paste(var_name, "ft", sep="")
+        col <-     predict(get(fit_name), test)
         results_df <- cbind(results_df, col)
         colnames(results_df)[i+1] <- var_name
     }
-
-    
+    output_file_loc <- paste(dir, save_as, ".csv", sep ="")    
+    write.csv(results_df, file = output_file_loc, row.names = FALSE)
+    resultS_df <<- results_df
 }
